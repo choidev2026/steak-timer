7d581c8856f5565abf204c1caad42d9e27c9828b
choidev2026 <choidev2026@github.com>
2026-07-13 15:01:21 +0900

    feature: WhileSubscribed(5초) 관용값 이유 주석 추가 (#8 리뷰)

--- feature/timer/src/main/kotlin/com/seriouschoi/steaktimer/feature/timer/TimerViewModel.kt
+++ feature/timer/src/main/kotlin/com/seriouschoi/steaktimer/feature/timer/TimerViewModel.kt
@@ -22,6 +22,9 @@ class TimerViewModel @Inject constructor(
         .map { it.toUiState() }
         .stateIn(
             scope = viewModelScope,
+            // 마지막 구독자(화면)가 떠난 뒤 5초 유예를 두고 상위를 멈춘다.
+            // 화면 회전(config change)이나 짧은 백그라운드처럼 순간적으로 구독이 끊길 때
+            // 상위 flow를 재시작하지 않게 하는 관용값. 오래 떠나면 멈춰 자원을 아낀다.
             started = SharingStarted.WhileSubscribed(5_000),
             initialValue = TimerUiState.INITIAL,
         )
